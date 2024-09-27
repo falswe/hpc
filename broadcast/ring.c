5ee73f446a7d4c5f4b82f0cebb8f08ccf43beeec
W. F. <we.f@posteo.net>
2024-09-27 09:36:32 +0200

    Implement ring broadcast

--- broadcast/ring.c
+++ broadcast/ring.c
@@ -1,27 +1,29 @@
 #include <mpi.h>
 #include <stdio.h>
-#include <string.h>
 
-int main(argc, argv) {
-  int n_processes = 0;
-  int id = 0;
-  int v = 0;
+int main() {
+    int n_processes = 0;
+    int id = 0;
+    int v = 0;
 
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &n_processes);
-  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-  do {
+    // ring-based with linear complexity
     if (id == 0) {
-      scanf("%d", v);
-      MPI_Send(&v, 1, MPI_INT, id + 1, 0, MPI_COMM_WORLD);
+        printf("send value %d to process %d\n", v, 1);
+        MPI_Send(&v, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
     } else {
-      MPI_Recv(&v, 1, MPI_INT, id - 1, 0, MPI_COMM_WORLD, &status);
-      if (id < n_processes - 1) {
-        // something is missing here
-      }
+        MPI_Recv(&v, 1, MPI_INT, id - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("received value %d on process %d\n", v, id - 1);
+
+        if (id != (n_processes - 1)) {
+            printf("send value %d to process %d\n", v, id + 1);
+            MPI_Send(&v, 1, MPI_INT, id + 1, 0, MPI_COMM_WORLD);
+        }
     }
-  }
-  MPI_Finalize();
-  return 0;
+
+    MPI_Finalize();
+    return 0;
 }
