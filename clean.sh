707cad8dd1c62878f3facec1ccff86cb6439c3dc
dongi01 <lorenzo.dongili@unitn.it>
2024-10-18 23:50:06 +0200

    clean.sh script fixed, before it was deleting git files

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 find ~ -type f -name "*.sh.e*" -delete
 find ~ -type f -name "*.sh.o*" -delete
-find ~ -type f ! -name "*.*" -delete
+#find ~ -type f ! -name "*.*" -delete
+find ~ -type f ! -name "*.*" -not \( -path '*/.*' \) -delete
+
